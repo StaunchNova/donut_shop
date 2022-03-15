978484f59938a5b72ae7ad06b9271b5128d3f076
Paschal <paschalkedoo@gmail.com>
2022-03-15 18:16:55 +0100

    Initial commit for donut shop

---
+++ lib/main.dart
@@ -0,0 +1,89 @@
+import 'package:donut_shop/widgets.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: SplashPage(),
+  ));
+}
+
+class SplashPage extends StatefulWidget {
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage>
+    with SingleTickerProviderStateMixin {
+  AnimationController? donutController;
+  Animation<double>? rotationAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    donutController =
+        AnimationController(duration: const Duration(seconds: 5), vsync: this)
+          ..repeat();
+    rotationAnimation = Tween<double>(begin: 0, end: 1).animate(
+        CurvedAnimation(parent: donutController!, curve: Curves.linear));
+  }
+
+  @override
+  void dispose() {
+    donutController!.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Future.delayed(const Duration(seconds: 10), () {
+      Navigator.of(context).pushReplacement(
+          MaterialPageRoute(builder: (context) => DonutShopMain()));
+    });
+    return Scaffold(
+      backgroundColor: Utils.mainColor,
+      body: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            RotationTransition(
+              turns: rotationAnimation!,
+              child: Image.network(
+                Utils.donutLogoWhiteNoText,
+                width: 100,
+                height: 100,
+              ),
+            ),
+            Image.network(
+              Utils.donutLogoWhiteText,
+              width: 150,
+              height: 150,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Utils {
+  static const Color mainColor = Color(0xFFFF0F7E);
+  static const Color mainDark = Color(0xFF980346);
+  static const String donutLogoWhiteNoText =
+      'https://romanejaquez.github.io/flutter-codelab4/assets/donut_shop_logowhite_notext.png';
+  static const String donutLogoWhiteText =
+      'https://romanejaquez.github.io/flutter-codelab4/assets/donut_shop_text_reversed.png';
+  static const String donutLogoRedText =
+      'https://romanejaquez.github.io/flutter-codelab4/assets/donut_shop_text.png';
+  static const String donutTitleFavorites =
+      'https://romanejaquez.github.io/flutter-codelab4/assets/donut_favorites_title.png';
+  static const String donutTitleMyDonuts =
+      'https://romanejaquez.github.io/flutter-codelab4/assets/donut_mydonuts_title.png';
+  static const String donutPromo1 =
+      'https://romanejaquez.github.io/flutter-codelab4/assets/donut_promo1.png';
+  static const String donutPromo2 =
+      'https://romanejaquez.github.io/flutter-codelab4/assets/donut_promo2.png';
+  static const String donutPromo3 =
+      'https://romanejaquez.github.io/flutter-codelab4/assets/donut_promo3.png';
+}
