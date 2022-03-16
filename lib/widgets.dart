4ab3315e8138ae08581f5aab2eaf7c91b5421c78
Paschal <paschalkedoo@gmail.com>
2022-03-16 22:26:56 +0100

    Adding appBar

--- lib/widgets.dart
+++ lib/widgets.dart
@@ -1,10 +1,22 @@
 import 'package:flutter/material.dart';
 
+import 'main.dart';
+
 class DonutShopMain extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
+    return Scaffold(
+      appBar: AppBar(
+        iconTheme: const IconThemeData(color: Utils.mainDark),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        centerTitle: true,
+        title: Image.network(
+          Utils.donutLogoRedText,
+          width: 120,
+        ),
+      ),
+      body: const Center(
         child: Text('Hello, Donut Shop!'),
       ),
     );
